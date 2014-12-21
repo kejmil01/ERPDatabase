d4eb84d0bad6441cd494809e8b62abd7b5a70664
kejmil01 <kejmil01@gmail.com>
2014-12-21 14:07:04 +0100

    Update triggerów

---
+++ scripts/21-12-2014/5_Update_Procedury.sql
@@ -0,0 +1,18 @@
+USE ERPDatabase;
+
+DROP PROCEDURE PrzeniesUmowyDoHistorii;
+
+CREATE PROCEDURE PrzeniesUmowyDoHistorii(@IdPracownika INT) AS
+BEGIN
+		INSERT INTO Historia_Umowy
+		SELECT id_pracownicy, id_typ_umowy, id_typ_stanowiska, kwota, id_umowy
+		FROM Umowy
+		WHERE id_pracownicy = @IdPracownika;
+		
+		DELETE FROM Umowy
+		WHERE id_pracownicy = @IdPracownika;
+
+		UPDATE Pracownicy
+		SET aktywny = 0
+		WHERE id_pracownicy = @IdPracownika;
+END
---
+++ scripts/21-12-2014/6_Update_Trigger_Usuwania.sql
@@ -0,0 +1,25 @@
+DROP TRIGGER UsuwaniePracownika;
+
+CREATE TRIGGER UsuwaniePracownika
+ON Umowy
+INSTEAD OF DELETE
+AS
+	BEGIN
+		DECLARE @IdPracownika int;
+		SET @IdPracownika = (SELECT id_pracownicy  FROM deleted);
+
+		INSERT INTO Historia_Umowy
+		SELECT id_pracownicy, id_typ_umowy, id_typ_stanowiska, kwota, id_umowy
+		FROM Umowy
+		WHERE id_pracownicy = @IdPracownika;
+		
+		DELETE FROM Umowy
+		WHERE id_pracownicy = @IdPracownika;
+
+		UPDATE Pracownicy
+		SET aktywny = 0
+		WHERE id_pracownicy = @IdPracownika;
+
+		SELECT id_pracownicy, id_typ_umowy, id_typ_stanowiska, kwota
+		FROM deleted;
+	END
